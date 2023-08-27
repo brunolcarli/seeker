342991334f9826b07e781c0014201c3bf1e84db8
brunolcarli <brunolcarli@gmail.com>
2023-08-27 16:56:35 -0300

    CHANGE version

--- seeker/settings.py
+++ seeker/settings.py
@@ -15,7 +15,7 @@ import uuid
 from django.utils.log import DEFAULT_LOGGING
 
 
-VERSION = '0.0.0'
+VERSION = '0.0.1'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
